fa600d9e8c896b02b3ea6e136fbdf9373dc22eff
AdaMaldonado <34352705+AdaMaldonado@users.noreply.github.com>
2022-01-14 14:08:28 -0500

    Add some html

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="control-panel">
+        <button id="addRows">Add Row</button>
+        <button id="addColumns">Add Column</button>
+
+        <label htmlFor="clrSelection">Color Selection</label>
+        <select id="clrSelection">
+          <option>None</option>
+          <option value="blue" id="op1">Blue</option>
+          <option value="red" id="op2">Red</option>
+          <option value="yellow" id="op3">Yellow</option>
+        </select>
+      </div>
     </div>
   );
 }
