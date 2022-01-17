671acd482d88539e43f01c30ca0cd1c1db14f143
AdaMaldonado <34352705+AdaMaldonado@users.noreply.github.com>
2022-01-17 13:25:44 -0500

    Add rows and columns

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Table from "./Table";
 
-function App() {
-  return (
-    <div className="App">
-      <div id="control-panel">
-        <button id="addRows">Add Row</button>
-        <button id="addColumns">Add Column</button>
+class App extends React.Component {
 
-        <label htmlFor="clrSelection">Color Selection</label>
-        <select id="clrSelection">
-          <option>None</option>
-          <option value="blue" id="op1">Blue</option>
-          <option value="red" id="op2">Red</option>
-          <option value="yellow" id="op3">Yellow</option>
-        </select>
-      </div>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      row: 0,
+      column: 1,
+      color: ""
+    };
+  }
+
+  createRow = () => {
+    this.setState({row: this.state.row + 1})
+  }
+
+  createColumn = () => {
+    this.setState({column: this.state.column + 1})
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <div id="control-panel">
+            <button id="addRows" onClick={this.createRow}>Add Row</button>
+            <button id="addColumns" onClick={this.createColumn}>Add Column</button>
+            <label htmlFor="clrSelection">Color Selection</label>
+
+            <select id="clrSelection">
+              <option value="transparent">None</option>
+              <option value="blue" id="op1">Blue</option>
+              <option value="red" id="op2">Red</option>
+              <option value="yellow" id="op3">Yellow</option>
+            </select>
+          </div>
+
+          <div className="grid">
+            <Table
+              row={this.state.row}
+              column={this.state.column}
+              />
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/TableRow.js
@@ -0,0 +1,24 @@
+import React from "react";
+import TableCell from "./TableCell";
+import './App.css';
+
+class TableRow extends React.Component {
+
+    render() {
+        const columnArr = []
+        if (this.props.column === 0) {
+            columnArr.push(<TableCell/>)
+        } else {
+            for (let i = 0; i < this.props.column; i++) {
+                columnArr.push(<TableCell/>)
+            }
+        }
+        return (
+          <div className="row">
+              {columnArr}
+          </div>
+        );
+    }
+}
+
+export default TableRow;
